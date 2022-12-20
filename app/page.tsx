8ec0ebb1d85bc4249446d780c9652f4c70634b91
Nicolas Rouanne <nico.rouanne@gmail.com>
2022-12-20 14:27:09 +0100

    feat(cover): add background image
    - style using Next.js responsive image example
      - demo: https://image-component.nextjs.gallery/responsive
      - code: https://github.com/vercel/next.js/blob/66408466ad9a9aee555381b0a632a65f6dc01abb/examples/image-component/pages/responsive.tsx#L9-L19

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,40 @@
+import Image from "next/image";
+
+import smilingFace from "../public/images/smiling-face.svg";
+import heroBackground from "../public/images/hero-background.png";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">
-        La seule agence web dont vous avez besoin
-      </h1>
-      <p>
-        Qraft est une agence de développement web qui vous accompagne dans la
-        construction de votre application, API ou infrastructure technique.
-      </p>
-      <p>
-        Notre équipe composée d&quot;experts de plus de 10 années
-        d&quot;expérience met l&quot;accent sur la qualité et la performance de
-        votre service, dans la durée.
-      </p>
-      <button>Nous contacter</button>
+      <div>
+        <div className="absolute inset-x-0 top-0 -z-10">
+          <Image
+            src={heroBackground}
+            alt="Blue and pink linear gradient Hero background"
+            placeholder="blur"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </div>
+        <div>
+          <h1 className="text-3xl font-bold underline">
+            La seule agence web dont vous avez besoin
+          </h1>
+          <p>
+            Qraft est une agence de développement web qui vous accompagne dans
+            la construction de votre application, API ou infrastructure
+            technique.
+          </p>
+          <p>
+            Notre équipe composée d&quot;experts de plus de 10 années
+            d&quot;expérience met l&quot;accent sur la qualité et la performance
+            de votre service, dans la durée.
+          </p>
+          <button>Nous contacter</button>
+        </div>
+        <div>
+          <Image src={smilingFace} alt="Smiling doodle face" />
+        </div>
+      </div>
     </main>
   );
 }
