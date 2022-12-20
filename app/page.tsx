a9262f2a09ead106f3c6ec12eaaedd4c7aa673a4
Nicolas Rouanne <nico.rouanne@gmail.com>
2022-12-20 15:19:06 +0100

    feat(cover): position elements correctly

--- app/page.tsx
+++ app/page.tsx
@@ -15,24 +15,26 @@ export default function Home() {
             style={{ width: "100%", height: "auto" }}
           />
         </div>
-        <div>
-          <h1 className="text-3xl font-bold underline">
-            La seule agence web dont vous avez besoin
-          </h1>
-          <p>
-            Qraft est une agence de développement web qui vous accompagne dans
-            la construction de votre application, API ou infrastructure
-            technique.
-          </p>
-          <p>
-            Notre équipe composée d&quot;experts de plus de 10 années
-            d&quot;expérience met l&quot;accent sur la qualité et la performance
-            de votre service, dans la durée.
-          </p>
-          <button>Nous contacter</button>
-        </div>
-        <div>
-          <Image src={smilingFace} alt="Smiling doodle face" />
+        <div className="flex flex-row">
+          <div>
+            <h1 className="text-3xl font-bold underline">
+              La seule agence web dont vous avez besoin
+            </h1>
+            <p>
+              Qraft est une agence de développement web qui vous accompagne dans
+              la construction de votre application, API ou infrastructure
+              technique.
+            </p>
+            <p>
+              Notre équipe composée d&quot;experts de plus de 10 années
+              d&quot;expérience met l&quot;accent sur la qualité et la
+              performance de votre service, dans la durée.
+            </p>
+            <button>Nous contacter</button>
+          </div>
+          <div className="flex flex-1 justify-center">
+            <Image src={smilingFace} alt="Smiling doodle face" />
+          </div>
         </div>
       </div>
     </main>
