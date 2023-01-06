b498e34683876790a968135d846b205f2e5d710a
Nicolas Rouanne <nico.rouanne@gmail.com>
2023-01-06 10:09:17 +0100

    fix(StackGroup): add `flex-wrap` for children
    - so logos wrap when there are too many, instead of overflowing the screen size
    - move the spacing from the parent component to the children, so it doesn't behave weird when wrapping (e.g. having left margin on the first item of a row)
    - it seems it's not possible to conditionally apply styles to wrapped elements
    https://stackoverflow.com/questions/65634843/can-i-apply-css-to-a-flex-item-when-it-wraps-onto-a-new-row

--- app/StackGroup.tsx
+++ app/StackGroup.tsx
@@ -7,11 +7,11 @@ interface StackGroupProps {
 
 export default function StackGroup({ title, children }: StackGroupProps) {
   return (
-    <div className="flex flex-col pb-8">
+    <div className="flex flex-col">
       <h4 className="pb-4 font-serif text-sm font-bold uppercase text-violet-300">
         {title}
       </h4>
-      <div className="flex space-x-6">{children}</div>
+      <div className="flex flex-wrap items-start">{children}</div>
     </div>
   );
 }
