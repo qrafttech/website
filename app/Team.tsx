477e0e15b0f67bb8387e26f9f29b85f8382b7fd6
Nicolas Rouanne <nico.rouanne@gmail.com>
2023-01-09 12:40:37 +0100

    refactor(Team): adjust positioning and size for mobile

--- app/Team.tsx
+++ app/Team.tsx
@@ -8,8 +8,8 @@ import marseille from "../public/images/marseille.svg";
 
 export default function Team() {
   return (
-    <div className="grid grid-cols-2">
-      <div className="col-span-2 lg:col-span-1">
+    <div className="grid grid-cols-2 py-16">
+      <div className="col-span-full lg:col-span-1">
         <Image
           src={dinosaur}
           alt="Illustration of a dinosaur screaming 'Whoa!'"
@@ -34,40 +34,43 @@ export default function Team() {
           plus, dans la <strong>bonne humeur</strong>&nbsp;!
         </p>
       </div>
-      <div className="col-span-2 grid grid-cols-2 gap-8 p-8 lg:col-span-1">
-        <div className="col-span-full row-span-2 flex sm:col-span-1">
-          <Image
-            src={patioNicolas}
-            alt="Picture of Nicolas from above in a the patio with a laptop looking surprised"
-            placeholder="blur"
-            className="border-8 border-white object-cover drop-shadow-lg"
-            width={640}
-          />
+      <div className="col-span-full flex flex-col justify-end lg:col-span-1">
+        <div className="grid grid-cols-2 gap-2 max-xl:pt-8 sm:max-lg:gap-8 lg:gap-4 lg:px-8">
+          <div className="row-span-2 flex sm:col-span-1">
+            <Image
+              src={patioNicolas}
+              alt="Picture of Nicolas from above in a the patio with a laptop looking surprised"
+              placeholder="blur"
+              className="border-8 border-white object-cover drop-shadow-lg"
+              width={640}
+            />
+          </div>
+          <div className="flex sm:col-span-1">
+            <Image
+              src={officeMehdiNicolas}
+              alt="Picture of Mehdi and Nicolas looking at code on a laptop screen in an office"
+              placeholder="blur"
+              className="border-8 border-white object-cover"
+              width={640}
+            />
+          </div>
+          <div className="flex sm:col-span-1">
+            <Image
+              src={patioAlexisLili}
+              alt="Picture of Alexis and Lili with a laptop smiling in the patio"
+              placeholder="blur"
+              className="border-8 border-white object-cover"
+              width={640}
+            />
+          </div>
         </div>
-        <div className="col-span-full flex sm:col-span-1">
-          <Image
-            src={officeMehdiNicolas}
-            alt="Picture of Mehdi and Nicolas looking at code on a laptop screen in an office"
-            placeholder="blur"
-            className="border-8 border-white object-cover"
-            width={640}
-          />
-        </div>
-        <div className="col-span-full flex sm:col-span-1">
-          <Image
-            src={patioAlexisLili}
-            alt="Picture of Alexis and Lili with a laptop smiling in the patio"
-            placeholder="blur"
-            className="border-8 border-white object-cover"
-            width={640}
-          />
-        </div>
-        <div className="col-span-full flex flex-1">
-          <Image
-            src={marseille}
-            alt="Handwriting of 'Marseille' with a location pinpoint and a curly arrow"
-          />
-        </div>
+      </div>
+      <div className="col-span-full flex justify-center pt-4 lg:col-span-1 lg:col-start-2">
+        <Image
+          src={marseille}
+          alt="Handwriting of 'Marseille' with a location pinpoint and a curly arrow"
+          className="max-w-[50%]"
+        />
       </div>
     </div>
   );
