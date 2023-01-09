eeb12fc4d20fd9a685c81abfefce9fe3cf2fc22f
Nicolas Rouanne <nico.rouanne@gmail.com>
2023-01-09 12:35:13 +0100

    refactor(Team): use a mobile first approch for CSS grid
    - rather than relying on `max-{}`, makes it more intuitive

--- app/Team.tsx
+++ app/Team.tsx
@@ -9,7 +9,7 @@ import marseille from "../public/images/marseille.svg";
 export default function Team() {
   return (
     <div className="grid grid-cols-2">
-      <div className="max-lg:col-span-2">
+      <div className="col-span-2 lg:col-span-1">
         <Image
           src={dinosaur}
           alt="Illustration of a dinosaur screaming 'Whoa!'"
@@ -34,8 +34,8 @@ export default function Team() {
           plus, dans la <strong>bonne humeur</strong>&nbsp;!
         </p>
       </div>
-      <div className="grid grid-cols-2 gap-8 p-8 max-lg:col-span-2">
-        <div className="row-span-2 flex max-sm:col-span-full">
+      <div className="col-span-2 grid grid-cols-2 gap-8 p-8 lg:col-span-1">
+        <div className="col-span-full row-span-2 flex sm:col-span-1">
           <Image
             src={patioNicolas}
             alt="Picture of Nicolas from above in a the patio with a laptop looking surprised"
@@ -44,7 +44,7 @@ export default function Team() {
             width={640}
           />
         </div>
-        <div className="flex max-sm:col-span-full">
+        <div className="col-span-full flex sm:col-span-1">
           <Image
             src={officeMehdiNicolas}
             alt="Picture of Mehdi and Nicolas looking at code on a laptop screen in an office"
@@ -53,7 +53,7 @@ export default function Team() {
             width={640}
           />
         </div>
-        <div className="flex max-sm:col-span-full">
+        <div className="col-span-full flex sm:col-span-1">
           <Image
             src={patioAlexisLili}
             alt="Picture of Alexis and Lili with a laptop smiling in the patio"
