1915fca83ff744528db6b88f9ca760a4d499e4f7
Nicolas Rouanne <nico.rouanne@gmail.com>
2023-01-11 15:58:43 +0100

    feat(head): add meta tags
    - generated using https://metatags.io/

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,30 @@
 export default function Head() {
+  const title = "Qraft - La seule agence web dont vous avez besoin";
+  const url = "https://qraft.tech";
+  const description =
+    "Qraft est une agence de développement qui vous accompagne dans la construction de votre application web, mobile, API ou infrastructure technique.";
+  const thumbnail = "/favicons/thumbnail.png";
+
   return (
     <>
-      <title>Qraft</title>
+      {/* Primary Meta Tags */}
+      <title>{title}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={thumbnail} />
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={thumbnail} />
+      {/* Favicons */}
       <link rel="icon" href="/favicon.ico" />
       <link
         rel="apple-touch-icon"
