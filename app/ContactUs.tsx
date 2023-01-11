c778cbe4030fe0d735f0b11b227788b986ac7302
Nicolas Rouanne <nico.rouanne@gmail.com>
2023-01-11 17:09:48 +0100

    feat(ContactUs): add `mailto` with subject
    - see https://stackoverflow.com/a/4782084/5186918 for subject

--- app/ContactUs.tsx
+++ app/ContactUs.tsx
@@ -3,15 +3,20 @@ import clsx from "clsx";
 type ContactUsProps = Partial<Pick<HTMLDivElement, "className">>;
 
 export default function ContactUs({ className }: ContactUsProps) {
+  const email = "hello@qraft.tech";
+  const subject = "Hello Qraft - Un petit mail après être allé sur votre site";
+
   return (
-    <button
-      type="button"
-      className={clsx(
-        "rounded-2xl border border-white bg-transparent font-medium text-white hover:bg-white/20",
-        className
-      )}
-    >
-      Nous contacter
-    </button>
+    <a href={`mailto:${email}?subject=${subject}`}>
+      <button
+        type="button"
+        className={clsx(
+          "rounded-2xl border border-white bg-transparent font-medium text-white hover:bg-white/20",
+          className
+        )}
+      >
+        Nous contacter
+      </button>
+    </a>
   );
 }
