2fdd9dc1221be1df9bbead7c548d513853e74cf8
Nicolas Rouanne <nico.rouanne@gmail.com>
2023-01-11 17:11:42 +0100

    feat(Menu): hide sections on mobile

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -5,10 +5,10 @@ export default function Menu() {
     <div className="flex items-center justify-between text-white">
       <span className="font-serif text-2xl font-extrabold">Qraft</span>
       <div className="flex items-center space-x-10">
-        <span>Services</span>
-        <span>Expertises</span>
-        <span>Équipe</span>
-        <span>Projets</span>
+        <span className="max-md:hidden">Services</span>
+        <span className="max-md:hidden">Expertises</span>
+        <span className="max-md:hidden">Équipe</span>
+        <span className="max-md:hidden">Projets</span>
         <ContactUs className="px-3 py-2 text-sm max-md:rounded-xl md:px-4 md:py-3 md:text-base" />
       </div>
     </div>
