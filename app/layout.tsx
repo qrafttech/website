68637827b69d530a24fe00abe37b71e39ddf61f3
Nicolas Rouanne <nico.rouanne@gmail.com>
2023-01-13 15:31:18 +0100

    feat(scroll): add smooth scrolling
    - using `scroll-behavior: smooth;` https://developer.mozilla.org/en-US/docs/Web/CSS/scroll-behavior
    - https://tailwindcss.com/docs/scroll-behavior
    - this answer https://stackoverflow.com/a/31731329/5186918 and this fiddle https://jsfiddle.net/hf4gap4v/ helped a lot

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="fr">
+    <html lang="fr" className="scroll-smooth">
       <head />
       <body
         className={`${montserrat.variable} ${sora.variable} ${scribbleNote.variable} font-sans font-medium`}
