289d76c564afeea464d6545153a8859e9f27ea4b
Nicolas Rouanne <nico.rouanne@gmail.com>
2023-01-27 11:52:28 +0100

    fix(font size): add more line-height according to design

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
   theme: {
     extend: {
       fontSize: {
+        sm: ["0.875rem", "1.5rem"],
+        lg: ["1.125rem", "2rem"],
         base: ["1rem", "1.75rem"],
+        "2xl": ["1.5rem", "2.25rem"],
+        "4xl": ["2.25rem", "2.75rem"],
+        "5xl": ["3rem", "3.75rem"],
+        "6xl": ["3.75rem", "4.5rem"],
       },
       fontFamily: {
         sans: ["var(--font-montserrat)", ...fontFamily.sans],
