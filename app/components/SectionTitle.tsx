51a379266efc7c4267ab62c5f27a6151b7156e6f
Nicolas Rouanne <nico.rouanne@gmail.com>
2023-01-27 14:08:38 +0100

    fix(SectionTitle): smaller font-size on mobile

--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -12,7 +12,7 @@ export default function SectionTitle({
   return (
     <h2
       className={clsx(
-        "bg-gradient-to-r bg-clip-text font-serif text-5xl font-bold text-transparent",
+        "bg-gradient-to-r bg-clip-text font-serif text-4xl font-bold text-transparent lg:text-5xl",
         className
       )}
     >
