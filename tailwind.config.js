09160bab01010adae333325b515e1916c5134989
Nicolas Rouanne <nico.rouanne@gmail.com>
2023-02-06 14:19:13 +0100

    chore(tailwind): change tailwind rendering directory
    - since we are now rendering `pages/`

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontSize: {
