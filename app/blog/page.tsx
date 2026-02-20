ed3c88acc752edf87c5966339448223fe45377c5
Adrien Lupo <adrien.lupo@protonmail.com>
2026-02-20 17:12:22 +0100

    refactor: remove premature error/loading/pagination from blog
    These will be added back with the Notion integration.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,24 +1,9 @@
 import FluidContainer from "../../components/FluidContainer";
 import SectionTitle from "../../components/SectionTitle";
 import BlogCard from "../components/BlogCard";
-import Pagination from "./components/Pagination";
 import { articles } from "./data";
 
-const PAGE_SIZE = 10;
-
-interface BlogPageProps {
-  searchParams: Promise<{ page?: string }>;
-}
-
-export default async function BlogPage({ searchParams }: BlogPageProps) {
-  const { page } = await searchParams;
-  const totalPages = Math.ceil(articles.length / PAGE_SIZE);
-  const currentPage = Math.max(1, Math.min(Number(page) || 1, totalPages));
-  const pageArticles = articles.slice(
-    (currentPage - 1) * PAGE_SIZE,
-    currentPage * PAGE_SIZE
-  );
-
+export default function BlogPage() {
   return (
     <FluidContainer>
       <div className="pt-16 pb-24">
@@ -26,7 +11,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
           Nos articles
         </SectionTitle>
         <div className="py-12">
-          {pageArticles.map((article) => (
+          {articles.map((article) => (
             <BlogCard
               key={article.slug}
               slug={article.slug}
@@ -36,9 +21,6 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
             />
           ))}
         </div>
-        {totalPages > 1 && (
-          <Pagination totalPages={totalPages} currentPage={currentPage} />
-        )}
       </div>
     </FluidContainer>
   );
